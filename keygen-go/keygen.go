47cabca4601c77e32ec199dcdc6a0362c5f60b89
Olivier Borderies <olivier.borderies@gmail.com>
2023-03-05 08:59:29 +0100

    Make go kengen parallel -> same speed as C++

--- keygen-go/keygen.go
+++ keygen-go/keygen.go
@@ -8,21 +8,368 @@ import (
 	"path"
 	"runtime"
 	"strings"
+	"sync"
 	"time"
 )
 
 func main() {
 
 	// fast
-	genSuitKeys(40)
-	genFiveFlushKeys(13)
-	genSevenFlushKeys(13)
+	// genSuitKeys(40)
+	// genFiveFlushKeys(13)
+	// genSevenFlushKeys(13)
 
 	// about 1mn
-	genFiveFaceKeys(13)
+	// genFiveFaceKeys(13)
 
 	// about 2h
-	genSevenFaceKeys(13)
+	// genSevenFaceKeys(13)
+
+	// parallel
+	// genFiveFaceKeysParallel(13)
+	genSevenFaceKeysParallel(13)
+
+}
+
+func dynamicSearchRange(n int) int {
+	a := n / 40
+	if a < 100 {
+		return 100
+	}
+	if a > 20_000 {
+		return 20_000
+	}
+	return a
+}
+
+// ---------------------------------
+// ---------------------------------
+// ---------------Key Gen Face Keys - Seven - Parallel
+
+func genSevenFaceKeysSearch(curKeys *[13]int, k int, tStart int, tEnd int, n int, wg *sync.WaitGroup, ch chan [2]int) {
+
+	defer wg.Done()
+
+	sumKeys := make([]int, 50000)
+	LARGE_INT := int(1e9)
+
+	var keys [13]int
+	for i := 0; i < k; i++ {
+		keys[i] = curKeys[i]
+	}
+
+	valid := false
+	t := tStart
+
+	for !valid && t < tEnd {
+		keys[k] = t
+		c := 0
+		for c1 := 0; c1 < k+1; c1++ {
+			for c2 := c1; c2 < k+1; c2++ {
+				for c3 := c2; c3 < k+1; c3++ {
+					for c4 := c3; c4 < k+1; c4++ {
+						for c5 := c4; c5 < k+1; c5++ {
+							for c6 := c5; c6 < k+1; c6++ {
+								for c7 := c6; c7 < k+1; c7++ {
+									if (c1 != c5) && (c2 != c6) && (c3 != c7) {
+										sumKeys[c] = keys[c1] + keys[c2] + keys[c3] + keys[c4] + keys[c5] + keys[c6] + keys[c7]
+										c++
+
+									}
+								}
+							}
+						}
+
+					}
+				}
+			}
+		}
+
+		i := 0
+		valid = true
+		for (valid) && (i < c-1) {
+			j := i + 1
+			for (valid) && (j < c) {
+				if sumKeys[i] == sumKeys[j] {
+					valid = false
+					t++
+				}
+				j++
+			}
+			i++
+		}
+		if valid {
+			// fmt.Printf("go rountine %d return %d\n", n, keys[k])
+			ch <- [2]int{n, keys[k]}
+			return
+		}
+
+	}
+	ch <- [2]int{n, LARGE_INT}
+}
+
+func genSevenFaceKeysParallel(nbKeys int) {
+	// generate keys for faces 1, 2, 3,.., 9, T, J, Q, K, A
+	// keys are such that the sums of any 2 combinations of 7 faces (with max 4 repetition) are distincts
+	// (discarding all other card info)
+
+	fmt.Println(" ")
+	defer track(runningtime("genSevenFaceKeys"))
+
+	if nbKeys == 0 {
+		nbKeys = 13
+	}
+
+	var keys [13]int
+	LARGE_INT := int(1e9)
+
+	// bootstrapping - empirical
+	keys[0] = 0
+	keys[1] = 1
+	keys[2] = 5
+	k := 3
+	fmt.Printf("bootstrapping -> k=%d: keys=%v\n", k, keys)
+
+	nCoRoutine := runtime.GOMAXPROCS(runtime.NumCPU())
+	fmt.Printf("nCoRoutine=%d\n", nCoRoutine)
+
+	t0 := time.Now()
+	tInit := keys[k-1] + 1
+	searchRange := dynamicSearchRange(tInit)
+	fmt.Printf("searchRange=%d\n", searchRange)
+
+	for k < nbKeys {
+		fmt.Printf("--------START LOOP \n")
+		wg := sync.WaitGroup{}
+		ch := make(chan [2]int, nCoRoutine)
+
+		for x := 0; x < nCoRoutine; x++ {
+			wg.Add(1)
+			tStart := tInit + x*searchRange
+			tEnd := tInit + (x+1)*searchRange - 1
+			fmt.Printf("lanch go rountine %d tStart=%d tEnd=%d \n", x+1, tStart, tEnd)
+			go genSevenFaceKeysSearch(&keys, k, tStart, tEnd, x+1, &wg, ch)
+
+		}
+		go func() {
+			wg.Wait()
+			close(ch)
+		}()
+		fmt.Printf("---------AFTER WAIT\n")
+
+		nextKey := LARGE_INT
+		routines := make([]bool, nCoRoutine)
+		for response := range ch {
+			r := response[0] - 1
+			candKey := response[1]
+			fmt.Printf("receive go rountine %d candKey=%d\n", r+1, candKey)
+			if candKey != LARGE_INT {
+			}
+			routines[r] = true
+			if candKey < nextKey {
+				nextKey = candKey
+			}
+			test := 0
+			for q := 0; q < r+1; q++ {
+				if routines[q] {
+					test += 1
+				}
+			}
+			fmt.Printf("DEBUG routines=%v test=%d r+1=%d\n", routines, test, r+1)
+			if test == r+1 && candKey == nextKey && nextKey != LARGE_INT {
+				fmt.Printf("DEBUG BREAK\n")
+				break
+			}
+		}
+
+		t1 := time.Now()
+		dt := t1.Sub(t0)
+
+		if nextKey == LARGE_INT {
+			fmt.Printf("FINAL: no key found -> carry on\n")
+			fmt.Printf("k=%d: keys=%v - t=%.2f s\n", k, keys, dt.Seconds())
+			tInit += nCoRoutine * searchRange
+			searchRange = dynamicSearchRange(tInit)
+			fmt.Printf("searchRange=%d\n", searchRange)
+
+		} else {
+			fmt.Printf("FINAL: nextKey=%d\n", nextKey)
+			keys[k] = nextKey
+			fmt.Printf("k=%d: keys=%v - t=%.2f s\n", k, keys, dt.Seconds())
+
+			k++
+			tInit = keys[k-1] + 1
+			searchRange = dynamicSearchRange(tInit)
+			fmt.Printf("searchRange=%d\n", searchRange)
+
+		}
+	}
+
+	var solutions [][]int
+	solutions = append(solutions, keys[:])
+	saveResult(solutions, "seven-face-keys-parallel")
+	fmt.Println("done")
+}
+
+// ---------------------------------
+// ---------------------------------
+// ---------------Key Gen Face Keys - Five - Parallel
+
+func genFiveFaceKeysSearch(curKeys *[13]int, k int, tStart int, tEnd int, n int, wg *sync.WaitGroup, ch chan [2]int) {
+
+	defer wg.Done()
+
+	sumKeys := make([]int, 50000)
+	LARGE_INT := int(1e9)
+
+	var keys [13]int
+	for i := 0; i < k; i++ {
+		keys[i] = curKeys[i]
+	}
+
+	valid := false
+	t := tStart
+
+	for !valid && t < tEnd {
+		keys[k] = t
+		c := 0
+		for c1 := 0; c1 < k+1; c1++ {
+			for c2 := c1; c2 < k+1; c2++ {
+				for c3 := c2; c3 < k+1; c3++ {
+					for c4 := c3; c4 < k+1; c4++ {
+						for c5 := c4; c5 < k+1; c5++ {
+							if c1 != c5 {
+								sumKeys[c] = keys[c1] + keys[c2] + keys[c3] + keys[c4] + keys[c5]
+								c++
+							}
+						}
+					}
+				}
+			}
+		}
+
+		i := 0
+		valid = true
+		for (valid) && (i < c-1) {
+			j := i + 1
+			for (valid) && (j < c) {
+				if sumKeys[i] == sumKeys[j] {
+					valid = false
+					t++
+				}
+				j++
+			}
+			i++
+		}
+		if valid {
+			fmt.Printf("go rountine %d return %d\n", n, keys[k])
+			ch <- [2]int{n, keys[k]}
+			return
+		}
+	}
+	ch <- [2]int{n, LARGE_INT}
+}
+
+func genFiveFaceKeysParallel(nbKeys int) {
+	// generate keys for faces 1, 2, 3,.., 9, T, J, Q, K, A
+	// keys are such that the sums of any 2 combinations of 5 faces (with max 4 repetition) are distincts
+	// (discarding all other card info)
+
+	fmt.Println(" ")
+	defer track(runningtime("genFiveFaceKeysParallel"))
+
+	if nbKeys == 0 {
+		nbKeys = 13
+	}
+
+	var keys [13]int
+	LARGE_INT := int(1e9)
+
+	// bootstrapping
+	keys[0] = 0
+	keys[1] = 1
+	k := 2
+	fmt.Printf("bootstrapping -> k=%d: keys=%v\n", k, keys)
+
+	nCoRoutine := runtime.GOMAXPROCS(runtime.NumCPU())
+	fmt.Printf("nCoRoutine=%d\n", nCoRoutine)
+
+	t0 := time.Now()
+	tInit := keys[k-1] + 1
+	searchRange := dynamicSearchRange(tInit)
+	fmt.Printf("searchRange=%d\n", searchRange)
+
+	for k < nbKeys {
+		fmt.Printf("--------START LOOP \n")
+		wg := sync.WaitGroup{}
+		ch := make(chan [2]int, nCoRoutine)
+
+		for x := 0; x < nCoRoutine; x++ {
+			wg.Add(1)
+			tStart := tInit + x*searchRange
+			tEnd := tInit + (x+1)*searchRange - 1
+			fmt.Printf("lanch go rountine %d tStart=%d tEnd=%d \n", x+1, tStart, tEnd)
+			go genFiveFaceKeysSearch(&keys, k, tStart, tEnd, x+1, &wg, ch)
+
+		}
+		go func() {
+			wg.Wait()
+			close(ch)
+		}()
+		fmt.Printf("---------AFTER WAIT\n")
+
+		nextKey := LARGE_INT
+		routines := make([]bool, nCoRoutine)
+		for response := range ch {
+			r := response[0] - 1
+			candKey := response[1]
+			fmt.Printf("receive go rountine %d candKey=%d\n", r+1, candKey)
+			if candKey != LARGE_INT {
+			}
+			routines[r] = true
+			if candKey < nextKey {
+				nextKey = candKey
+			}
+			test := 0
+			for q := 0; q < r+1; q++ {
+				if routines[q] {
+					test += 1
+				}
+			}
+			fmt.Printf("DEBUG routines=%v test=%d r+1=%d\n", routines, test, r+1)
+			if test == r+1 && candKey == nextKey && nextKey != LARGE_INT {
+				fmt.Printf("DEBUG BREAK\n")
+				break
+			}
+		}
+
+		t1 := time.Now()
+		dt := t1.Sub(t0)
+
+		if nextKey == LARGE_INT {
+			fmt.Printf("FINAL: no key found -> carry on\n")
+			fmt.Printf("k=%d: keys=%v - t=%.2f s\n", k, keys, dt.Seconds())
+			tInit += nCoRoutine * searchRange
+			searchRange = dynamicSearchRange(tInit)
+			fmt.Printf("searchRange=%d\n", searchRange)
+
+		} else {
+			fmt.Printf("FINAL: nextKey=%d\n", nextKey)
+			keys[k] = nextKey
+			fmt.Printf("k=%d: keys=%v - t=%.2f s\n", k, keys, dt.Seconds())
+
+			k++
+			tInit = keys[k-1] + 1
+			searchRange = dynamicSearchRange(tInit)
+			fmt.Printf("searchRange=%d\n", searchRange)
+
+		}
+	}
+
+	var solutions [][]int
+	solutions = append(solutions, keys[:])
+	saveResult(solutions, "five-face-keys-parallel")
+	fmt.Println("done")
 }
 
 // ---------------------------------
@@ -390,7 +737,7 @@ func genSevenFlushKeys(nbKeys int) {
 
 // ---------------------------------
 // ---------------------------------
-// ---------------Key Gen Face Keys - Five
+// ---------------Key Gen Face Keys - Seven
 
 func genSevenFaceKeys(nbKeys int) {
 	// generate keys for faces 1, 2, 3,.., 9, T, J, Q, K, A
